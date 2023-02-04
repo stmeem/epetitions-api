812aeaaeb674fc0c39a4c4a447ba6d13d786141b
stmeem <stmeem@yahoo.com>
2023-02-04 21:32:35 +0600

    Set up laravel application

---
+++ database/factories/PetitionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Petition;
+use Faker\Generator as Faker;
+
+$factory->define(Petition::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
