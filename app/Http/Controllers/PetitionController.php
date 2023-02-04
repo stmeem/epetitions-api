1ace243cfc3ab991b823b85f90ce3c655625f504
stmeem <stmeem@yahoo.com>
2023-02-04 23:46:16 +0600

    Create RESTful api

--- app/Http/Controllers/PetitionController.php
+++ app/Http/Controllers/PetitionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PetitionCollection;
+use App\Http\Resources\PetitionResource;
 use App\Petition;
 use Illuminate\Http\Request;
 
@@ -14,51 +16,63 @@ class PetitionController extends Controller
      */
     public function index()
     {
-        //
+        $petitions = Petition::all();
+//        return PetitionResource::collection($petitions);
+        return response()->json(new PetitionCollection($petitions), 200);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+
+        $petition = Petition::create($request->only([
+            'title', 'description', 'category', 'author', 'signee'
+        ]));
+
+        return new PetitionResource($petition);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Petition  $petition
-     * @return \Illuminate\Http\Response
+     * @param \App\Petition $petition
+     * @return PetitionResource
      */
     public function show(Petition $petition)
     {
-        //
+        return new PetitionResource($petition);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Petition  $petition
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Petition $petition
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Petition $petition)
     {
-        //
+        $petition = Petition::update($request->only([
+            'title', 'description', 'category', 'author', 'signee'
+        ]));
+
+        return new PetitionResource($petition);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Petition  $petition
+     * @param \App\Petition $petition
      * @return \Illuminate\Http\Response
      */
     public function destroy(Petition $petition)
     {
-        //
+        $petition->delete();
+        return response()->json(null, 204);
     }
 }
